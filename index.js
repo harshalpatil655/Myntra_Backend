5e36f398026688b366746655c6b0a9c4032a9ca1
harshalpatil655 <harshalpatil655@gmail.com>
2022-12-23 19:45:16 +0530

    addedport

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { connection } = require("./Config/db.js");
 const { UserRoute } = require("./Routes/User.route.js");
+require("dotenv").config();
 
 const app = express();
 const cors = require("cors");
+let PORT = process.env.PORT;
 const { MensdataRoute } = require("./Routes/Mensdata.route.js");
 const { CartRoute } = require("./Routes/Cart.route.js");
 const { Authetication } = require("./Middleware/authetication.js");
@@ -15,12 +17,12 @@ app.use("/data", MensdataRoute);
 app.use(Authetication);
 app.use("/cart", CartRoute);
 
-app.listen(8080, async () => {
+app.listen(PORT, async () => {
   try {
     await connection;
     console.log("connected to DB");
   } catch (err) {
     console.log("Connection Failed to Connect DB");
   }
-  console.log("Listening on port 8080");
+  console.log(`Listening on port ${PORT}`);
 });
